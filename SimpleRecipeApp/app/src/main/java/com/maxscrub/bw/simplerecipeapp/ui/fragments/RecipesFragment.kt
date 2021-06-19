28f1ea71022245be2c08b5360df9f29907646687
BW <bmwanz@live.com>
2021-06-19 13:22:16 -0700

    retrofit models & refactor

--- SimpleRecipeApp/app/src/main/java/com/maxscrub/bw/simplerecipeapp/ui/fragments/RecipesFragment.kt
+++ SimpleRecipeApp/app/src/main/java/com/maxscrub/bw/simplerecipeapp/ui/fragments/RecipesFragment.kt
@@ -1,20 +1,25 @@
-package com.maxscrub.bw.simplerecipeapp
+package com.maxscrub.bw.simplerecipeapp.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.maxscrub.bw.simplerecipeapp.R
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
-
-class FavoriteRecipesFragment : Fragment() {
+class RecipesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view =  inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        view.recyclerview.showShimmer()
+
+        return view
     }
 
 }
